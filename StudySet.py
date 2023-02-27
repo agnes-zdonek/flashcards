3f453d0279d470be39655eda3ae9314368c89fec
Zdonek Agnieszka <agnieszka.zdonek@etu.sorbonne-universite.fr>
2023-02-27 12:17:00 +0100

    scraped json methods, added read method from json to study set, added a new test file

--- StudySet.py
+++ StudySet.py
@@ -1,12 +1,19 @@
 from Flashcard import * 
+import json
 
 class StudySet: 
     def __init__(self) -> None:
         self.set_of_flashcards = set()
     
-    def add_flashcard(flashcard : Flashcard) -> None:
+    def add_flashcard(self, flashcard : Flashcard) -> None:
         self.set_of_flashcards.add(flashcard)
 
     def print_set(self) -> None:
         print(self.set_of_flashcards)
     
+    def read_flashcards(self, file_path : str) -> None:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            flashcards = data['flashcards']
+            for card in flashcards:
+                study_set.add_flashcard(Flashcard(card['question'], card['answer']))
---
+++ tests.py
@@ -0,0 +1,5 @@
+from JsonMethods import *
+
+
+set_general_knowledge = StudySet()
+JsonMethods.read_flashcards('data.json', set_general_knowledge)
